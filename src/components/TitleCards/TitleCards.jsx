9acd8a3686085cfc09577a18675d586cfa2cccf6
imkashishkathuria <kathuriak805@gmail.com>
2025-01-09 18:49:09 +0530

    display data from tmdb api

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,10 +1,22 @@
 import './TitleCards.css';
-import cards_data from '../../assets/cards/Cards_data';
-import { useRef } from 'react';
+// import cards_data from '../../assets/cards/Cards_data';  `
+import { useRef, useState } from 'react';
 import { useEffect } from 'react';
 
 const TitleCards = ({title, category}) => {
+
+  const [apiData, setApiData] = useState([]);
   const cardsRef = useRef();
+  // const api = "a800701f0bcfcafdfad587b25f415f6d";
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhODAwNzAxZjBiY2ZjYWZkZmFkNTg3YjI1ZjQxNWY2ZCIsIm5iZiI6MTczNjMzODkwMC44ODksInN1YiI6IjY3N2U2ZGQ0ZTBkNTY0OTNlNGJiMDc3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1s9bbljwa4xZrAReoJS7Y_eq-fH4lz4IYPasfuxVrX8'
+    }
+  };
+  
 
   const handleWheel = (event)=>{
     event.preventDefault();
@@ -12,24 +24,43 @@ const TitleCards = ({title, category}) => {
   }
 
   useEffect(()=>{
+    fetch('https://api.themoviedb.org/3/movie/now_playing?language=en-US&page=2', options)
+    .then((res) => {
+      if(!res.ok){
+        throw new Error(`HTTP error! Status: ${res.status}`)
+      }
+      console.log("Response status:", res.status); 
+      return res.json()
+  })
+    .then((data)=>{
+      console.log(data);
+      setApiData(data.results || []);
+    })
+    .catch(err => console.error(err));
+
+    console.log(apiData)
+     
     cardsRef.current.addEventListener('wheel', handleWheel)  
-  },[])
+  },[options])
   return (
     <div className='title-cards'>
       <h2>
-        {title?title:" Popular on Netfix"} 
+        {title ? title : "Popular on Netfix"} 
       </h2>
       <div className='card-list' ref={cardsRef}>
+        
         {
-          cards_data.map((card, index)=>{
-            return <div className='card' key={index}>
+          apiData.length > 0 ? (apiData.map((card, index)=>(
+            <div className='card' key={index}>
 
-              <img src={card.image} alt='' />
-              <p>{card.name}</p>
-
+              <img src={`https://image.tmdb.org/t/p/w500`+card.backdrop_path} alt='kk' />
+              <p>{card.original_title}</p>
               </div>
-          })
-        }
+          ))
+      ):(null
+
+      )}
+
       </div>
     </div>
   )
